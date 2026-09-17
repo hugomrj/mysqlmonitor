c0050b953f138da7b4d88ec87d13db5cb1e13268
hugo romero <hugomrj@gmail.com>
2026-09-17 12:37:50 -0300

    set17 1230

--- main.py
+++ main.py
@@ -269,10 +269,6 @@ async def root():
     return FileResponse("static/index.html")
 
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
-
-
 
 if __name__ == "__main__":
     
